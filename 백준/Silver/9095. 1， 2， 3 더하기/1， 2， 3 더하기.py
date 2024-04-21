6e8404c966a8a4c1b04e0bf1f6cb67ef5b45822b
bckim9489 <47053587+bckim9489@users.noreply.github.com>
2024-04-21 16:22:16 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,18 +1,18 @@
 import sys
 t = int(input())
 
-def selected2(cnt, sum, n):
-    if sum > n:
+def selected2(res, n):
+    if res > n:
         return 0
-    if sum == n:
+    if res == n:
         return 1
     
     now = 0
 
     for i in range(1, 4):
-        now += selected2(cnt+1, sum+i, n)
+        now += selected2(res+i, n)
     
     return now
 
 for _ in range(t):
-    print(selected2(0, 0, int(input())))
+    print(selected2(0, int(input())))
