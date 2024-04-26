c1153a049486850408922c42b39b15b883a7f458
bckim9489 <47053587+bckim9489@users.noreply.github.com>
2024-04-26 17:52:43 +0900

    [Gold III] Title: Guess, Time: 2776 ms, Memory: 118160 KB -BaekjoonHub

--- 백준/Gold/1248. Guess/Guess.py
+++ 백준/Gold/1248. Guess/Guess.py
@@ -2,32 +2,32 @@ import sys
 
 n = int(input())
 a = str(input())
-graph = []
-tmp = 0
-cnt = 0
+graph = [[0 for _ in range(n)] for _ in range(n)]
 
 res = [0 for _ in range(n)]
+cnt = 0
 
-for i in range(n, 0, -1):
-    row = []
-    for j in range(cnt):
-        row.append("")
-    for j in range(i):
-        row.append(a[j+tmp])
-    graph.append(row)
-    tmp += i
-    cnt += 1
+for i in range(n):
+    for j in range(i,n):
+        if a[cnt] == '0':
+            graph[i][j] = 0
+        elif a[cnt] == '+':
+            graph[i][j] = 1
+        else:
+            graph[i][j] = -1
+        cnt += 1
+    
 
 def good(arr, idx):
     for i in range(0, idx+1):
         data = sum(arr[i:idx+1])
-        if graph[i][idx] == "-":         
+        if graph[i][idx] == -1:         
             if data >= 0:
                 return False
-        if graph[i][idx] == "+":
+        if graph[i][idx] == 1:
             if data <= 0:
                 return False
-        if graph[i][idx] == "0":
+        if graph[i][idx] == 0:
             if data != 0:
                 return False
     return True
@@ -41,19 +41,23 @@ def selected(idx):
     start = 0
     end = 0
 
-    if graph[idx][idx] == "+":
+    if graph[idx][idx] == 1:
         start = 1
         end = 10
 
-    if graph[idx][idx] == "-":
+    if graph[idx][idx] == -1:
         start = -10
         end = -1
-
-    for i in range(start, end+1):
-        res[idx] = i
-        if idx > 0:
-            if good(res, idx) == False:
-                continue
+    
+    if graph[idx][idx] == 0:
+        res[idx] = 0
         selected(idx+1)
+    else :
+        for i in range(start, end+1):
+            res[idx] = i
+            if idx > 0:
+                if good(res, idx) == False:
+                    continue
+            selected(idx+1)
 
 selected(0)
