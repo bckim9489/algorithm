b5f3afd60395817c7b12daacd2dd09895f313018
bckim9489 <47053587+bckim9489@users.noreply.github.com>
2024-04-26 17:38:23 +0900

    [Gold III] Title: Guess, Time: 3052 ms, Memory: 120480 KB -BaekjoonHub

---
+++ 백준/Gold/1248. Guess/Guess.py
@@ -0,0 +1,62 @@
+import sys
+
+n = int(input())
+a = str(input())
+graph = []
+tmp = 0
+cnt = 0
+
+res = [0 for _ in range(n)]
+
+for i in range(n, 0, -1):
+    row = []
+    for j in range(cnt):
+        row.append("")
+    for j in range(i):
+        row.append(a[j+tmp])
+    graph.append(row)
+    tmp += i
+    cnt += 1
+
+def good(arr, idx):
+    for i in range(0, idx+1):
+        for j in range(idx, idx+1):
+            if i > j:
+                continue
+            data = sum(arr[i:j+1])
+            if graph[i][j] == "-":         
+                if data >= 0:
+                    return False
+            if graph[i][j] == "+":
+                if data <= 0:
+                    return False
+            if graph[i][j] == "0":
+                if data != 0:
+                    return False
+    return True
+            
+
+def selected(idx):
+    if idx == n :
+        print(*res, sep=" ")
+        sys.exit()
+    
+    start = 0
+    end = 0
+
+    if graph[idx][idx] == "+":
+        start = 1
+        end = 10
+
+    if graph[idx][idx] == "-":
+        start = -10
+        end = -1
+
+    for i in range(start, end+1):
+        res[idx] = i
+        if idx > 0:
+            if good(res, idx) == False:
+                continue
+        selected(idx+1)
+
+selected(0)
