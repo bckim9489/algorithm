ac6d5ee263fc7755828633de4837ef986653bca9
bckim9489 <47053587+bckim9489@users.noreply.github.com>
2024-05-02 17:36:05 +0900

    [Silver II] Title: 부분수열의 합, Time: 340 ms, Memory: 110572 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,19 +1,14 @@
 import sys
-from itertools import combinations
 
 n, s = map(int, sys.stdin.readline().split())
-input_arr = list(map(int, sys.stdin.readline().split()))
+l = list(map(int, sys.stdin.readline().split()))
+ans = 0
+for i in range(1, (1<<n)):
+    res = 0
+    for k in range(n):
+        if i&(1<<k):
+            res += l[k]
+    if res == s:
+        ans += 1
 
-cnt = 0
-
-def foo(n):
-    global cnt
-    for i in range(1,n+1):
-        a = combinations(input_arr,i)
-        for j in a:
-            b = list(j)
-            if sum(b) == s:
-                cnt += 1    
-    print(cnt)
-
-foo(n)
+print(ans)
