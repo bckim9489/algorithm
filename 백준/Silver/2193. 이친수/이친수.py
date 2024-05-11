20f78dbff462bd187837ec5f0fe53cedeb2e630e
bckim9489 <47053587+bckim9489@users.noreply.github.com>
2024-05-11 17:50:16 +0900

    [Silver III] Title: 이친수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(input())
+
+d = [[0, 0] for _ in range(n+1)]
+d[1][1] = 1
+
+ans = 0
+
+for i in range(2, n+1):
+    d[i][0] = d[i-1][0]+ d[i-1][1]
+    d[i][1] = d[i-1][0]
+
+print(sum(d[n]))
