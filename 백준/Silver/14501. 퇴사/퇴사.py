fc62064feea37552dbd79739dee0d746114d9bef
bckim9489 <47053587+bckim9489@users.noreply.github.com>
2024-05-14 13:43:46 +0900

    [Silver III] Title: 퇴사, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,23 +1,23 @@
 import sys
 
 n = int(input())
-a = []
-ans = 0
+t = [0 for _ in range(n+2)]
+p = [0 for _ in range(n+2)]
+d = [-1 for _ in range(n+2)]
 
-for i in range(n):
-    t, p = map(int, sys.stdin.readline().split())
-    a.append((t,p))
+for i in range(1, n+1):
+    t[i], p[i] = map(int, sys.stdin.readline().split())
 
-def selected(day, cost):
-    global ans
-    if day == n:
-        ans = max(ans, cost)
-        return
-    if day > n:
-        return
-    
-    selected(day+1, cost)
-    selected(day+a[day][0], cost+a[day][1])
+def selected(day):
+    if day == n+1:
+        return 0
+    if day > n+1:
+        return -sys.maxsize
+    if d[day] != -1:
+        return d[day]
+    x_res = selected(day+1)
+    o_res = p[day] + selected(day+t[day])
+    d[day] = max(x_res, o_res)
+    return d[day]
 
-selected(0, 0)
-print(ans)
+print(selected(1))
